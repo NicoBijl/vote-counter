8d7682808ede6c4fc3c31de53cb63bf60934fa44
nicobijl <n.bijl@portofrotterdam.com>
2026-03-10 16:33:55 +0100

    chore(ci): force CI run with code change to verify deploy preview (#148)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import './App.css'
 import MainContainer from "./components/MainContainer.tsx";
 
 
+// Root component of the application
 function App() {
+    
     return (
         <>
             <MainContainer/>
